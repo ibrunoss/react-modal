ce56e5b85fb8273752847748eb7809173b7468b7
Bruno Silva Santana <ibrunoss@outlook.com>
2021-11-20 14:41:29 -0300

    Feat: Apply Modal
     - Set Modal component in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import Modal from "./Modal/Modal";
 import ModalButton from "./ModalButton/ModalButton";
 
 function App() {
-  return <ModalButton />;
+  const [showModal, setShowModal] = useState(false);
+
+  const changeModal = () => {
+    setShowModal((v) => !v);
+  };
+
+  return (
+    <>
+      <ModalButton onClick={changeModal} />
+      <Modal isOpen={showModal} onChange={changeModal}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>Modal Content</h1>
+        </div>
+      </Modal>
+    </>
+  );
 }
 
 export default App;
